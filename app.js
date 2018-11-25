334bf7f1a5177794ac957003fe7bec80ca21abce
Nick <nikkurz@gmail.com>
2018-11-25 13:54:34 -0700

    added click function with if else

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeHello() {
+  if (document.getElementById('hello').innerHTML  === 'Hello World!'){
+    document.getElementById('hello').innerHTML = 'I was changed'
+  }
+  else (document.getElementById('hello').innerHTML = 'Hello World!')
+  }
