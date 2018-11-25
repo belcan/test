e7f221dfd8f4fe51c74f3633493de4a8c63fa3f2
Nick <nikkurz@gmail.com>
2018-11-25 14:24:49 -0700

    changed to capital letters I Was Changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function changeHello() {
   if (document.getElementById('hello').innerHTML  === 'Hello World!'){
-    document.getElementById('hello').innerHTML = 'I Ws Changed'
+    document.getElementById('hello').innerHTML = 'I Was Changed'
   }
   else (document.getElementById('hello').innerHTML = 'Hello World!')
   }
