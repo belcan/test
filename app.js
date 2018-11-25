5b923bd54729e38da03439cec4a7069a04e620f6
Nick <nikkurz@gmail.com>
2018-11-25 14:22:27 -0700

    added text-allign

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function changeHello() {
   if (document.getElementById('hello').innerHTML  === 'Hello World!'){
-    document.getElementById('hello').innerHTML = 'I was changed'
+    document.getElementById('hello').innerHTML = 'I Ws Changed'
   }
   else (document.getElementById('hello').innerHTML = 'Hello World!')
   }
