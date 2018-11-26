ee1d4de27c5a787e7a0185ce3a90ce4b4d6b5b4f
Nick <nikkurz@gmail.com>
2018-11-25 22:03:22 -0700

    initial commit for another branch

---
+++ app2.js
@@ -0,0 +1,6 @@
+function another() {
+  document.getElementById('hello').innerHTML === 'Hello World!' ?
+  document.getElementById('hello').innerHTML = 'I was changed' :
+  document.getElementById('hello').innerHTML = 'Hello World';
+
+}
